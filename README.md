ba5930389a83bf807c40bf79560ae893f266d8fb
hamibot <52420549+hamibot@users.noreply.github.com>
2022-08-25 16:36:04 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Hamibot for Visual Studio Code
+
+Hamibot 智能提示、调试。
+
+[Hamibot](https://hamibot.com/) 是一款安卓平台自动化工具，无需 root。
+
+<a href="https://hamibot.com/" ><img src="https://repository-images.githubusercontent.com/317106172/7d373900-616e-11eb-9170-ba931b11f1cb" alt="Hamibot" /></a>
+
+## 📖 使用
+
+1. 打开 VSCode 设置
+2. 在插件设置中找到 Hamibot
+3. 填写 Token、Script ID、Robot ID
+
+> Token 前往[控制台](https://hamibot.com/account/tokens)获取
+
+> Script ID、Robot ID 通过调用 [REST API](https://docs.hamibot.com/rest/overview) 获得
+
+## 🔗 链接
+
+- [官网](https://hamibot.com/)
+- [脚本市场](https://hamibot.com/marketplace/)
+- [快速上手](https://hamibot.com/guide/)
+- [开发文档](https://docs.hamibot.com/)
+- [博客](https://blog.hamibot.com/)
+- [REST API](https://docs.hamibot.com/rest/overview)
+- [GitHub](https://github.com/hamibot/hamibot)
+- [开源脚本](https://github.com/hamibot/awesome-hamibot)
---
+++ src/extension.ts
@@ -0,0 +1,130 @@
+import * as vscode from 'vscode';
+import fs = require('fs');
+import path = require('path');
+import fetch from 'node-fetch';
+import * as FormData from 'form-data';
+
+export function activate(context: vscode.ExtensionContext) {
+  const getConf = () => {
+    const config = vscode.workspace.getConfiguration();
+    const token = config.get('hamibot.token');
+    const scriptId = config.get('hamibot.scriptId');
+    const robotId = config.get('hamibot.robotId');
+    if (!token) {
+      vscode.window.showErrorMessage('需要填写 Token');
+      vscode.commands.executeCommand(
+        'workbench.action.openSettings',
+        'hamibot.token'
+      );
+      return;
+    }
+    if (!scriptId) {
+      vscode.window.showErrorMessage('需要填写 Script ID');
+      vscode.commands.executeCommand(
+        'workbench.action.openSettings',
+        'hamibot.scriptId'
+      );
+      return;
+    }
+    if (!robotId) {
+      vscode.window.showErrorMessage('需要填写 Robot ID');
+      vscode.commands.executeCommand(
+        'workbench.action.openSettings',
+        'hamibot.robotId'
+      );
+      return;
+    }
+    return { token, scriptId, robotId };
+  };
+
+  const run = vscode.commands.registerCommand('hamibot.run', async () => {
+    try {
+      const config = getConf();
+      if (!config) return;
+      const response = await fetch(
+        `https://api.hamibot.com/v1/devscripts/${config.scriptId}/run`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${config.token}`,
+          },
+          body: JSON.stringify({
+            robots: [
+              {
+                _id: config.robotId,
+                name: 'Hamibot VSCode',
+              },
+            ],
+          }),
+        }
+      );
+      if (response.ok) {
+        vscode.window.showInformationMessage('运行成功');
+      } else {
+        const data = await response.json();
+        vscode.window.showErrorMessage('运行失败：' + JSON.stringify(data));
+      }
+    } catch (e) {
+      console.error(e);
+      vscode.window.showErrorMessage('运行异常');
+    }
+  });
+
+  const saveAndRun = vscode.commands.registerCommand(
+    'hamibot.saveAndRun',
+    async () => {
+      try {
+        const config = getConf();
+        if (!config) return;
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) return;
+        const { fileName } = editor.document;
+        const ext = path.extname(fileName);
+        if (ext !== '.js') {
+          vscode.window.showErrorMessage('不是 .js 文件，无法保存');
+          return;
+        }
+        await editor.document.save();
+        const body = new FormData();
+        body.append('file', fs.readFileSync(fileName), {
+          contentType: 'application/javascript',
+          filename: path.basename(fileName),
+        });
+        const response = await fetch(
+          `https://api.hamibot.com/v1/devscripts/${config.scriptId}/files`,
+          {
+            method: 'PUT',
+            headers: {
+              Authorization: `token ${config.token}`,
+            },
+            body,
+          }
+        );
+        if (response.ok) {
+          //vscode.window.showInformationMessage('保存成功');
+          vscode.commands.executeCommand('hamibot.run');
+        } else {
+          const data = await response.json();
+          vscode.window.showErrorMessage('保存失败：' + JSON.stringify(data));
+        }
+      } catch (e) {
+        console.error(e);
+        vscode.window.showErrorMessage('保存异常');
+      }
+    }
+  );
+
+  const openSetting = vscode.commands.registerCommand(
+    'hamibot.openSetting',
+    async () => {
+      vscode.window.showInformationMessage('打开设置');
+      vscode.commands.executeCommand(
+        'workbench.action.openSettings',
+        'hamibot'
+      );
+    }
+  );
+
+  context.subscriptions.push(...[run, saveAndRun, openSetting]);
+}
